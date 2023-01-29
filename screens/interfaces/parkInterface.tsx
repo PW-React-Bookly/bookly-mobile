3e4f96d470c65f7a82147987686d4f6250bf5a98
Jakub Górka <kubagorka27@gmail.com>
2023-01-29 16:02:46 +0100

    Display parkings to rent

--- screens/interfaces/parkInterface.tsx
+++ screens/interfaces/parkInterface.tsx
@@ -3,5 +3,13 @@ import {BookableType} from "./bookingInterface";
 export interface ParkInterface {
     bookableType: BookableType
     description: string,
-    id: string
+    id: string,
+    name: string,
+    photo: string,
+    pricePerDay: number,
+    latitude: number,
+    longitude: number,
+    security: boolean,
+    parkingLotType: string,
+    capacity: number
 }
